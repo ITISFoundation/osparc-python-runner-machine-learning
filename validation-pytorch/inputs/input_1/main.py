91ecfc5878fc73175d6a9d29dd47e845b0aede49
Andrei Neagu <neagu@itis.swiss>
2025-05-23 15:39:58 +0200

    proper validation

--- validation-pytorch/inputs/input_1/main.py
+++ validation-pytorch/inputs/input_1/main.py
@@ -13,4 +13,3 @@ a = torch.tensor([1.0, 2.0, 3.0])
 b = torch.tensor([4.0, 5.0, 6.0])
 print("Sum:", a + b)
 
-
--- validation-tensorflow/inputs/input_1/main.py
+++ validation-tensorflow/inputs/input_1/main.py
@@ -9,7 +9,3 @@ print(response)
 
 msg = tf.constant("Hello, TensorFlow!")
 tf.print(msg)
-
-(Path(os.environ["OUTPUT_FOLDER"]) / "output_1" / "a_file.txt").write_text(
-    "Hello, TensorFlow!"
-)
